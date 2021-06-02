ce76e01ef3d854fb9ddfadf4ed699986175224a9
Cyril <cyril.goldenschue@cpnv.ch>
2021-06-02 15:59:57 +0200

    Add tabs

---
+++ code/app/src/main/java/ch/leytto/cynoclient/ui/customer/CustomerViewModel.kt
@@ -0,0 +1,13 @@
+package ch.leytto.cynoclient.ui.customer
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CustomerViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is customer Fragment"
+    }
+    val text: LiveData<String> = _text
+}
