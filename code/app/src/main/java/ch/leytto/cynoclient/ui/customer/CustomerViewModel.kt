c23122f1608aeaf84b148431ff6bfbac69788d83
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-06-04 14:25:11 +0200

    Try to compile

--- code/app/src/main/java/ch/leytto/cynoclient/ui/customer/CustomerViewModel.kt
+++ code/app/src/main/java/ch/leytto/cynoclient/ui/customer/CustomerViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class CustomerViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is customer Fragment"
+        value = "Hello there !"
     }
     val text: LiveData<String> = _text
 }
