6e0cf6c87a25135dd7b21ee575b835c249486184
Cyril <cyril.goldenschue@cpnv.ch>
2021-06-04 13:56:50 +0200

    hotfix bug with tab report

--- code/app/src/main/java/ch/leytto/cynoclient/MainActivity.kt
+++ code/app/src/main/java/ch/leytto/cynoclient/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_reports, R.id.nav_customers, R.id.nav_dogs, R.id.nav_settings), drawerLayout)
+                R.id.nav_home, R.id.nav_reports, R.id.nav_customers, R.id.nav_dogs, R.id.nav_services, R.id.nav_localities), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
