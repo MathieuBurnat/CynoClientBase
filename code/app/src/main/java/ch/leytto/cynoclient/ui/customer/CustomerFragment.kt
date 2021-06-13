c4b7f41b23f9b74ff280531768a64838f972a62f
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-06-13 15:14:13 +0200

    [Refactor] Display basics : Clients

--- code/app/src/main/java/ch/leytto/cynoclient/ui/customer/CustomerFragment.kt
+++ code/app/src/main/java/ch/leytto/cynoclient/ui/customer/CustomerFragment.kt
@@ -6,26 +6,36 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ch.leytto.cynoclient.ClientListAdapter
+import ch.leytto.cynoclient.CynoClientApplication
 import ch.leytto.cynoclient.R
+import ch.leytto.cynoclient.viewmodels.ClientViewModel
+import ch.leytto.cynoclient.viewmodels.ViewModelFactory
 
 class CustomerFragment : Fragment() {
 
-    private lateinit var reportViewModel: CustomerViewModel
+    private val clientViewModel: ClientViewModel by viewModels {
+        ViewModelFactory((requireActivity().application as CynoClientApplication).clientRepository)
+    }
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        reportViewModel =
-                ViewModelProvider(this).get(CustomerViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_customer, container, false)
-        val textView: TextView = root.findViewById(R.id.client_title)
-        reportViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val recyclerView = root.findViewById<RecyclerView>(R.id.client_recyclerview)
+        val adapter = ClientListAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        clientViewModel.AllClients.observe(viewLifecycleOwner) { clients ->
+            clients.let { adapter.submitList(it) }
+        }
+
         return root
     }
 }
